985bf7cca28038de7f286a530ab516a5764eaefd
Alvaro Franz <97749528+3v-afranz@users.noreply.github.com>
2022-06-12 14:51:27 +0200

    Apply WP Coding Standards

--- uninstall.php
+++ uninstall.php
@@ -3,15 +3,15 @@
 namespace plugin\clone_posts;
 
 // Die if WP_UNINSTALL_PLUGIN isn't defined
-if(!defined('WP_UNINSTALL_PLUGIN')) {
-    die('You are not allowed to call this page directly.');
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die( 'You are not allowed to call this page directly.' );
 }
 
 // If we are on a multisite installation clean up all sub-sites
 if ( is_multisite() ) {
 
-	foreach (get_sites(['fields'=>'ids']) as $blog_id) {
-		switch_to_blog($blog_id);
+	foreach ( get_sites( array( 'fields' => 'ids' ) ) as $current_iteration_blog_id ) {
+		switch_to_blog( $current_iteration_blog_id );
 		cleanup_settings_data();
 		restore_current_blog();
 	}
@@ -20,7 +20,7 @@ if ( is_multisite() ) {
 	cleanup_settings_data();
 }
 
-function cleanup_settings_data(){
+function cleanup_settings_data() {
 
 	// Plugin options
 	$options = array(
